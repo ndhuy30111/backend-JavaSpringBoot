a14d400aaa0bfc8af54c448c9348c95d7efba316
ndhuy30111 <dh51701452@student.stu.edu.vn>
2021-02-26 10:17:34 +0700

    Update

--- apiwebsite/src/main/java/com/springboot/apiwebsite/ApiwebsiteApplication.java
+++ apiwebsite/src/main/java/com/springboot/apiwebsite/ApiwebsiteApplication.java
@@ -2,9 +2,10 @@ package com.springboot.apiwebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiwebsiteApplication {
+public class ApiwebsiteApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiwebsiteApplication.class, args);
