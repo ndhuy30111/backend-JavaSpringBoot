46ed7a2d055f656c060b0365de8ef7c4342d67e4
ndhuy30111 <dh51701452@student.stu.edu.vn>
2021-03-08 17:02:23 +0700

    message

--- apiwebsite/src/main/java/com/springboot/apiwebsite/model/Message.java
+++ apiwebsite/src/main/java/com/springboot/apiwebsite/model/Message.java
@@ -1,5 +1,24 @@
 package com.springboot.apiwebsite.model;
 
-public class Message {
+import org.springframework.http.HttpStatus;
 
+public class Message {
+	String message;
+	HttpStatus status;
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+	
 }
